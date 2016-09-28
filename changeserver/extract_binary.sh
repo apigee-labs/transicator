860d6a6ad105e33b58537bdb407eea1ecb92f7e6
Gregory Brail <greg@apigee.com>
2016-09-28 14:57:39 -0700

    Change build process to go back to one top-level glide.yaml. Change intermediate Docker builds to pull ".." to use it.

--- changeserver/extract_binary.sh
+++ changeserver/extract_binary.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -f Dockerfile.changeserver -t proxy-changeserver .
+docker build -f Dockerfile.changeserver -t proxy-changeserver ..
 CID=$(docker run -d proxy-changeserver sh)
 docker cp ${CID}:/changeserver changeserver
 docker rmi -f proxy-changeserver
