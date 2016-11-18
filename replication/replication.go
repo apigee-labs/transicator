873ed502f74ec9ecb75f259beadd493f1a6229f8
Kris Wehner <kris@yelp.com>
2016-11-17 21:23:19 -0800

    Fix standby status update to match the docs

--- replication/replication.go
+++ replication/replication.go
@@ -592,10 +592,10 @@ func (r *Replicator) updateLSN(highLSN uint64, conn *pgclient.PgConnection) {
 	log.Debugf("Updating server with last LSN %d", highLSN)
 	om := pgclient.NewOutputMessage(pgclient.CopyDataOut)
 	om.WriteByte(byte(pgclient.StandbyStatusUpdate))
-	om.WriteUint64(highLSN)                          // last written to disk
-	om.WriteUint64(highLSN)                          // last flushed to disk
-	om.WriteUint64(highLSN)                          // last applied
-	om.WriteInt64(time.Now().UnixNano() - epoch2000) // Timestamp!
+	om.WriteUint64(highLSN+1)                          // last written to disk+1
+	om.WriteUint64(highLSN+1)                          // last flushed to disk+1
+	om.WriteUint64(highLSN+1)                          // last applied+1
+	om.WriteUint64((uint64)((time.Now().UnixNano()/1000) - epoch2000)) // Timestamp, in microseconds
 	om.WriteByte(0)
 
 	conn.WriteMessage(om)
