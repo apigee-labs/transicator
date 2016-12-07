52e2ac4bdf0154f86d7c891c75859426d00b7228
Gregory Brail <gregbrail@google.com>
2016-12-06 21:23:32 -0800

    Update licenses to match standards.

--- snapshotserver/snapshot.go
+++ snapshotserver/snapshot.go
@@ -1,3 +1,18 @@
+/*
+Copyright 2016 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package main
 
 import (
@@ -88,11 +103,6 @@ func GetScopeData(cid string, db *sql.DB) (b []byte, err error) {
 		SnapshotInfo: snapInfo,
 		Timestamp:    snapTime,
 	}
-	/* FIXME:
-	 * (1) The two SELECTS have to be part of single transaction.
-	 * (2) Can Snapshot server know the APID_CONFIG & APID_CONFIG_SCOPE schema?
-	 * These are independant of the plugins - so the assumption.
-	 */
 
 	tx, err := db.Begin()
 	if err != nil {
