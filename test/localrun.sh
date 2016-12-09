e2a2219ed3b4a6432e0cbd181166c21624bc2b59
Gregory Brail <gregbrail@google.com>
2016-12-09 12:50:51 -0800

    Add a test script for the combined test.

---
+++ test/localrun.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [ -z  ${TEST_PG_URL} ]
+then
+  echo "TEST_PG_URL" must be set
+  exit 2
+fi
+
+../bin/snapshotserver -u ${TEST_PG_URL} -t 12123 --key ./keys/clearkey.pem --cert ./keys/clearcert.pem &
+ssPid=$!
+
+../bin/changeserver -u ${TEST_PG_URL} -t 12124 -s unittestslot -d ./data --key ./keys/clearkey.pem --cert ./keys/clearcert.pem &
+csPid=$!
+
+CHANGE_HOST=localhost CHANGE_PORT=12124 \
+SNAPSHOT_HOST=localhost SNAPSHOT_PORT=12123 \
+go test
+
+curl http://localhost:12124/markdown
+
+kill ${csPid}
+kill ${ssPid}
+rm -rf ./data
