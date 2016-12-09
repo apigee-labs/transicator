1ba39380c1317f828dea8328c123107687ae78f1
Gregory Brail <gregbrail@google.com>
2016-12-09 11:27:56 -0800

    Support batched updates from replication.

--- changeserver/replicator.go
+++ changeserver/replicator.go
@@ -21,31 +21,47 @@ import (
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/apigee-labs/transicator/common"
+	"github.com/apigee-labs/transicator/storage"
 )
 
 const (
 	acknowledgeDelay = 500 * time.Millisecond
+	maxBatchSize = 100
 )
 
 func (s *server) runReplication(firstChange common.Sequence) {
 	var lastAck uint64
 	var lastChange common.Sequence
-	ackTimer := time.NewTimer(acknowledgeDelay)
+	var changeBatch []*common.Change
+
+	// We will wake up periodically to send acknowledgements -- don't want to overload
+	// this timer also lets us consolidate lots of changes into a batch.
+	ackTimer := time.NewTicker(acknowledgeDelay)
 
 	for {
 		select {
 		case change := <-s.repl.Changes():
-			newChange := s.handleChange(change, firstChange)
-			if newChange.Compare(lastChange) > 0 {
-				lastChange = newChange
+			changeBatch = append(changeBatch, change)
+			if len(changeBatch) >= maxBatchSize {
+				newChange := s.handleChanges(changeBatch, firstChange)
+				if newChange.Compare(lastChange) > 0 {
+					lastChange = newChange
+				}
+				changeBatch = nil
 			}
 
 		case <-ackTimer.C:
+			if len(changeBatch) > 0 {
+				newChange := s.handleChanges(changeBatch, firstChange)
+				if newChange.Compare(lastChange) > 0 {
+					lastChange = newChange
+				}
+				changeBatch = nil
+			}
 			if lastChange.LSN > lastAck {
 				lastAck = lastChange.LSN
 				s.repl.Acknowledge(lastAck)
 			}
-			ackTimer.Reset(acknowledgeDelay)
 
 		case stopped := <-s.stopChan:
 			if atomic.LoadInt32(&s.dropSlot) == 0 {
@@ -60,25 +76,36 @@ func (s *server) runReplication(firstChange common.Sequence) {
 	}
 }
 
-func (s *server) handleChange(c *common.Change, firstChange common.Sequence) common.Sequence {
+func (s *server) handleChanges(cb []*common.Change, firstChange common.Sequence) common.Sequence {
 
-	changeSeq := c.GetSequence()
+	var entryBatch []storage.Entry
+	var lastSeq common.Sequence
 
-	if changeSeq.Compare(firstChange) > 0 {
-		scope := getScope(c)
-		log.Debugf("Received change %d for scope %s", c.ChangeSequence, scope)
-		if c.Timestamp == 0 {
-			c.Timestamp = time.Now().Unix()
+	for _, c := range cb {
+		cs := c.GetSequence()
+		if cs.Compare(firstChange) > 0 {
+			if c.Timestamp == 0 {
+				c.Timestamp = time.Now().Unix()
+			}
+			e := storage.Entry{
+				Scope: getScope(c),
+				LSN: c.CommitSequence,
+				Index: c.CommitIndex,
+				Data: encodeChangeProto(c),
+			}
+			entryBatch = append(entryBatch, e)
+			lastSeq = cs
 		}
-		dataBuf := encodeChangeProto(c)
-		s.db.Put(
-			scope, c.CommitSequence, c.CommitIndex, dataBuf)
-		s.tracker.update(changeSeq, scope)
-
-	} else {
-		log.Debugf("Ignoring change %s which we already processed", changeSeq)
 	}
-	return changeSeq
+
+	s.db.PutBatch(entryBatch)
+	log.Debugf("Inserted a batch of %d changes", len(entryBatch))
+
+	for _, e := range entryBatch {
+		s.tracker.update(common.MakeSequence(e.LSN, e.Index), e.Scope)
+	}
+
+ return lastSeq
 }
 
 func getScope(c *common.Change) string {
